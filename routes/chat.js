de4273ee854ba94130043a090a2886a5facd3621
Takeshi Kunimi <takc923@gmail.com>
2013-10-31 01:03:13 +0900

    s/user_id/userId/

--- routes/chat.js
+++ routes/chat.js
@@ -2,11 +2,11 @@ module.exports = function() {
     this.route = function (app) {
         var self = this;
         app.get('/chat/:chatId([0-9]+)', function (req, res) {
-            if (req.session.user_id === undefined) {
+            if (req.session.userId === undefined) {
                 res.redirect('/');
                 return;
             }
-            res.render('chat', {user_id : req.session.user_id});
+            res.render('chat', {userId : req.session.userId});
         });
     };
 };
